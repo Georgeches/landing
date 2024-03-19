a1e588ed052046553c8f6fc5d838a642ee32070a
Georgeches <georgechesire83@gmail.com>
2024-03-19 11:25:31 +0300

    updates

--- src/components/Paragraph.js
+++ src/components/Paragraph.js
@@ -2,7 +2,7 @@ export default function Paragraph({paragraph}){
 
     return(
         paragraph.type==="text"?
-            <p className="mb-5">{paragraph.content}</p>
+            <p className="mb-5" style={{fontSize: "18px", lineHeight: "2"}}>{paragraph.content}</p>
         :
             <img className="mb-5 w-100" src={paragraph.content} alt="img"/>
     )

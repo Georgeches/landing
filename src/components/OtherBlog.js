787a038c642325b15a72af8a1df92edeef93427c
Georgeches <georgechesire83@gmail.com>
2024-03-19 11:35:09 +0300

    add link

--- src/components/OtherBlog.js
+++ src/components/OtherBlog.js
@@ -1,9 +1,12 @@
 export default function OtherBlog({blog}){
 
+    const link = `/blog/${blog?.id}`;
     return(
-        <div className="d-flex py-2 w-100" style={{borderTop: "1px solid rgb(207, 207, 207)", cursor: "pointer"}}>
-            <img className="me-2" src={blog?.image} alt="blog" height="50"/>
-            <p className="">{blog?.title}</p>
-        </div>
+        <a href={link} className="w-100">
+            <div key={blog?.id} className="d-flex py-2 px-2 w-100 other-blog" style={{borderTop: "1px solid rgb(207, 207, 207)", cursor: "pointer"}}>
+                <img className="me-2" src={blog?.image} alt="blog" height="50"/>
+                <p className="text-dark">{blog?.title}</p>
+            </div>
+        </a>
     )
 }
